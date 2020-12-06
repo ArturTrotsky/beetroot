ebc200b817ba6ba0e283394109c990e39504a9c2
Artur Trotsky <artur.trotsky@ukr.net>
2020-12-06 15:31:47 +0200

    add magic methods

--- classes/User.php
+++ classes/User.php
@@ -7,6 +7,8 @@ class User
     private $password;
     public $email;
     private $salt = 'dsfb32ewd2qa';
+    private $properties = ['id', 'name', 'email'];
+    private $cardnumber;
 
     public function __construct(string $name, string $password, string $email, int $id = null)
     {
@@ -17,6 +19,11 @@ class User
         $this->name = $name;
         $this->email = $email;
         $this->setPassword($password);
+    }
+
+    public function __destruct()
+    {
+        echo 'object is removed';
     }
 
     public function setPassword(string $password)
@@ -76,5 +83,40 @@ class User
         );
         $this->id = $db->lastInsertId();
         return $this->id;
+    }
+
+    public function __call($name, $arguments)
+    {
+        if (strpos($name, 'get') === 0) {
+            $param = strtolower(explode('get', $name)[1]);
+            if (in_array($param, $this->properties)) {
+                return $this->{$param};
+            }
+        }
+
+        if (strpos($name, 'set') === 0) {
+            $param = strtolower(explode('set', $name)[1]);
+            if (in_array($param, $this->properties)) {
+                $this->{$param} = $arguments[0];
+            }
+        }
+    }
+
+    public function __get(string $name): string
+    {
+        if ($name == 'password') {
+            return $this->getPassword();
+        } elseif ($name == 'cardnumber') {
+            return '****' . substr($this->name, -4);
+        }
+    }
+
+    public function __set(string $name, string $value): void
+    {
+        if ($name == 'password') {
+            $this->setPassword($value);
+        } else {
+            $this->name = $value;
+        }
     }
 }
--- lessonClasses01/index.php
+++ lessonClasses01/index.php
@@ -3,13 +3,31 @@ declare(strict_types=1);
 require_once 'config.php';
 require_once CLASSES_PATH . 'User.php';
 
-$user1 = new User('11', '11', '1emawil@email1');
-$userId1 = $user1->register($pdo);
+$user1 = new User('11', '11', 'email@email');
+
+/*регистрация пользователя в базе данных
+$userId1 = $user1->register($pdo);*/
+
+//использование __call
+/*$user1->setEmail('set_email@email');
+var_dump($user1->getEmail());*/
+
+//использование __get
+//var_dump($user1->password);
+
+//использование __set __get
+//$user1->password = '2222';
+//var_dump($user1->password);
+
+//использование __set __get
+//$user1->cardnumber = '12345678';
+//var_dump($user1->cardnumber);
 
 
+echo '<br><br>';
 //$isChanged = $user1->changePassword('12345678', '1111');
 //echo($isChanged ? 'password change to ' . $user1->getPassword() : 'password was incorrect');
 
-echo($user1->login('user@user', '12345678') ? 'you are logged' : 'email or password incorrect');
+// echo($user1->login('user@user', '12345678') ? 'you are logged' : 'email or password incorrect');
 
 
